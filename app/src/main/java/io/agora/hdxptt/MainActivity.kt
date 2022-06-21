51d4a143240101ceb10f10b57b3cbe7cb73eb47f
Shaun Hubbard <shaunhubbard2013@icloud.com>
2022-06-21 18:31:02 -0400

    crashlytics setup

--- app/src/main/java/io/agora/hdxptt/MainActivity.kt
+++ app/src/main/java/io/agora/hdxptt/MainActivity.kt
@@ -3,7 +3,9 @@ package io.agora.hdxptt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.agora.hdxptt.ui.theme.AgoraPTTDemoTheme
+import java.lang.RuntimeException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +34,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column {
+        Text(text = "Hello $name!")
+        Button(onClick = {  }) {
+            Text(text = "Crash It")
+        }
+    }
 }
 
 @Preview(showBackground = true)
